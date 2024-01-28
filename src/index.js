2bebf7d12e6010e3fb30f627b43d32f6916db485
mrityunjay <mrityunjay.83039@gmail.com>
2024-01-28 12:58:49 +0530

    setup context with reducer, initial state and StateProvider also wraped the app component in provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
-         <App />
-    </BrowserRouter>
-
+  <BrowserRouter>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </BrowserRouter>
 );
