162f7d24f9cf3e3fd197ac318a23ebecb10dd89a
mrityunjay <mrityunjay.83039@gmail.com>
2024-01-28 17:54:31 +0530

    header section done with logout functionality and mobile menu design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,8 +31,17 @@ export const Header = () => {
     }
   };
 
+  const logOut = () => {
+    setIsMenu(false);
+    localStorage.clear();
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
+
   return (
-    <header className="fixed z-50 w-screen p-6 px-16">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
       {/* Desktop and Tablet */}
       <div className="hidden md:flex w-full h-full gap-8">
         <Link to={"/"} className="flex items-center gap-2">
@@ -83,7 +92,10 @@ export const Header = () => {
                 </Link>
               )}
 
-              <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+              <p
+                onClick={logOut}
+                className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+              >
                 Logout <MdLogout />{" "}
               </p>
             </motion.div>
@@ -92,7 +104,66 @@ export const Header = () => {
       </div>
 
       {/* For Moile device */}
-      <div className="flex md:hidden">Mobile</div>
+      <div className="flex justify-between md:hidden">
+        <div className="relative flex items-center content-center">
+          <MdShoppingBasket className="text-textColor h-6 w-6" />
+          <div className="w-5 h-5 bg-red-500 rounded-full flex justify-center items-center absolute -top-0 -right-3">
+            <p className="text-white font-normal text-xs">1</p>
+          </div>
+        </div>
+        <Link to={"/"} className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover gap-2" alt="Logo" />
+          <p className="text-headingColor text-xl font-bold"> City</p>
+        </Link>
+        <div className="relative">
+          <motion.img
+            onClick={login}
+            whileTap={{ scale: 0.6 }}
+            src={user ? user.photoURL : userIcon}
+            alt="User"
+            className="w-8 h-8 drop-shadow-xl cursor-pointer rounded-full"
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0.6, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+            >
+              {user && user.email === "mrityunjay.83039@gmail.com" && (
+                <Link
+                  to={"/createItem"}
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                >
+                  New Item <MdAdd />
+                </Link>
+              )}
+
+              <ul className="flex flex-col">
+                <li className=" px-4 py-2 text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out  hover:bg-slate-100 cursor-pointer">
+                  Home
+                </li>
+                <li className=" px-4 py-2 text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out  hover:bg-slate-100 cursor-pointer">
+                  Menu
+                </li>
+                <li className=" px-4 py-2 text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out  hover:bg-slate-100 cursor-pointer">
+                  About Us
+                </li>
+                <li className=" px-4 py-2 text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out  hover:bg-slate-100 cursor-pointer">
+                  Service
+                </li>
+              </ul>
+
+              <p
+                onClick={logOut}
+                className="m-2 p-2 rounded-md shadow-md px-4 py-2 flex justify-center items-center gap-3 cursor-pointer bg-gray-200 hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base"
+              >
+                Logout <MdLogout />{" "}
+              </p>
+            </motion.div>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
